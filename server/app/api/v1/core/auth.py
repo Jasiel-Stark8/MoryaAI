64e42138a2bb17a2e57405eca8700789b279a3ce
jason <nftsingularity8@gmail.com>
2024-02-19 18:49:54 +0000

    auth

--- server/app/api/v1/core/auth.py
+++ server/app/api/v1/core/auth.py
@@ -1,87 +1,77 @@
+"""User Auth module"""
+from flask import Flask, jsonify, request
+import logging
 from models.users import User
 from models.articles import Article
 from models.platforms import Platform
+import bcrypt
 
+# Signup
+def signup(username, email, password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    user = User(
+        username=username,
+        email=email,
+        hashed_password=hashed_password,
+        profile_picture='',
+        subscription_status='active',
+        authentication_provider='local',
+        is_active=True
+    )
+    existing_user = User.objects(email=user.email).first()
+    if not existing_user:
+        User.save(user)
+        return jsonify({'User created successfully'}, 201)
+    else:
+        return jsonify({'User already exists'}, 400)
 
+# Login
+def login(email, password):
+    user = User.objects(email=email).first()
+    if user:
+        if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8')):
+            return jsonify({'Login successful'}, 201)
+        else:
+            return jsonify({'Invalid password'}, 400)
+    else:
+        return jsonify({'User not found'}, 400)
 
-# print('Creating User...')
-# user = User(
-#     username='admin',
-#     email='admin@morya.com',
-#     hashed_password='admin',
-#     profile_picture='admin.png',
-#     subscription_status='active',
-#     authentication_provider='local',
-#     is_active=True
-# )
-# existing_user = User.objects(email=user.email).first()
-# if not existing_user:
-#     User.save(user)
-# else:
-#     print('User already exists')
+# Google Authentication
+def google_auth(user_info):
+    # Store user_info to the database
+    # Example code:
+    user = User(
+        username=user_info['username'],
+        email=user_info['email'],
+        hashed_password='',
+        profile_picture=user_info['profile_picture'],
+        subscription_status='active',
+        authentication_provider='google',
+        is_active=True
+    )
+    existing_user = User.objects(email=user.email).first()
+    if not existing_user:
+        User.save(user)
+        return 'User created successfully'
+    else:
+        return 'User already exists'
 
-# print('Querying user')
-# user = User.objects(email='admin@morya.com').first()
-# print(user.to_json() if user else 'User not found')
-# print('Done.')
-
-# print('Updating User...')
-# User.objects(email='admin@morya.com').update_one(
-#     username='admina',
-#     hashed_password='admin100'
-# )
-
-# print('Updated username and password')
-# user = User.objects(email='admin@morya.com').first()
-# print(user.to_json() if user else 'User not found')
-
-# # ==============================================
-# print('Creating User 2...')
-# user = User(
-#     username='Morya',
-#     email='morayai@morya.com',
-#     hashed_password='morya120',
-#     profile_picture='morya.png',
-#     subscription_status='active',
-#     authentication_provider='google',
-#     is_active=True
-# )
-# existing_user = User.objects(email=user.email).first()
-# if not existing_user:
-#     User.save(user)
-# else:
-#     print('User already exists')
-# # ==============================================
-# # ==============================================
-# print('Creating User 3...')
-# user = User(
-#     username='Jason',
-#     email='Jason@morya.com',
-#     hashed_password='jason',
-#     profile_picture='jason.png',
-#     subscription_status='active',
-#     authentication_provider='linkedin',
-#     is_active=True
-# )
-# existing_user = User.objects(email=user.email).first()
-# if not existing_user:
-#     User.save(user)
-# else:
-#     print('User already exists')
-# # ==============================================
-
-# print('fetching all users...')
-# users = [user.to_json() for user in User.objects()]
-# print(users)
-
-# print(f'Pre-deletion count: {User.objects.count()}')
-
-# print('Deleting user...')
-# email='admin@morya.com'
-# existing_user = User.objects(email=email).first()
-# if existing_user:
-#     existing_user.delete()
-# else:
-#     print('You might have changed your email')
-
-# print(f'Post-deletion count: {User.objects.count()}')
+# LinkedIn Authentication
+def linkedin_auth(user_info):
+    # Store user_info to the database
+    # Example code:
+    user = User(
+        username=user_info['username'],
+        email=user_info['email'],
+        hashed_password='',
+        profile_picture=user_info['profile_picture'],
+        subscription_status='active',
+        authentication_provider='linkedin',
+        is_active=True
+    )
+    existing_user = User.objects(email=user.email).first()
+    if not existing_user:
+        User.save(user)
+        return 'User created successfully'
+    else:
+        return 'User already exists'
